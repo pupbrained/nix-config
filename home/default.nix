caccbd7894140bb8dd2959dcc55a3ce785245a3b
ReplayCoding <replaycoding@gmail.com>
2022-06-11 16:06:04 -0700

    NVFETCHER!

--- home/default.nix
+++ home/default.nix
@@ -173,14 +173,6 @@
         "100:class_g   ~=  'jetbrains'"
         "100:class_g   *?= 'slack'"
       ];
-      package = pkgs.picom.overrideAttrs (o: {
-        src = pkgs.fetchFromGitHub {
-          repo = "picom";
-          owner = "ibhagwan";
-          rev = "44b4970f70d6b23759a61a2b94d9bfb4351b41b1";
-          sha256 = "0iff4bwpc00xbjad0m000midslgx12aihs33mdvfckr75r114ylh";
-        };
-      });
     };
   };
 
