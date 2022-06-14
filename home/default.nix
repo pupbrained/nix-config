0df2ef5f3389c58d64739da18620094db4e3c5f9
marsupialgutz <mars@possums.xyz>
2022-06-14 17:58:11 -0400

    woo

--- home/default.nix
+++ home/default.nix
@@ -29,7 +29,6 @@
     jetbrains.idea-ultimate
     jq
     keychain
-    kitty
     kotatogram-desktop
     libnotify
     libsForQt5.qtstyleplugin-kvantum
@@ -184,6 +183,43 @@
         inactive_tab_font_style = "normal";
         adjust_line_height = 5;
         adjust_column_width = 0;
+        foreground = "#CDD6F4";
+        background = "#1E1E2E";
+        selection_foreground = "#1E1E2E";
+        selection_background = "#F5E0DC";
+        cursor = "#F5E0DC";
+        cursor_text_color = "#1E1E2E";
+        url_color = "#B4BEFE";
+        active_border_color = "#CBA6F7";
+        inactive_border_color = "#8E95B3";
+        bell_border_color = "#EBA0AC";
+        active_tab_foreground = "#11111B";
+        active_tab_background = "#CBA6F7";
+        inactive_tab_foreground = "#CDD6F4";
+        inactive_tab_background = "#181825";
+        tab_bar_background = "#11111B";
+        mark1_foreground = "#1E1E2E";
+        mark1_background = "#87B0F9";
+        mark2_foreground = "#1E1E2E";
+        mark2_background = "#CBA6F7";
+        mark3_foreground = "#1E1E2E";
+        mark3_background = "#74C7EC";
+        color0 = "#45475A";
+        color8 = "#45475A";
+        color1 = "#F38BA8";
+        color9 = "#F38BA8";
+        color2 = "#A6E3A1";
+        color10 = "#A6E3A1";
+        color3 = "#F9E2AF";
+        color11 = "#F9E2AF";
+        color4 = "#89B4FA";
+        color12 = "#89B4FA";
+        color5 = "#F5C2E7";
+        color13 = "#F5C2E7";
+        color6 = "#94E2D5";
+        color14 = "#94E2D5";
+        color7 = "#BAC2DE";
+        color15 = "#BAC2DE";
       };
     };
   };
