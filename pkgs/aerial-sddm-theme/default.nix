c2f687dc0801966d94065f1dc6ba7b042c501740
ReplayCoding <replaycoding@gmail.com>
2022-06-20 13:40:19 -0700

    wtf

--- pkgs/aerial-sddm-theme/default.nix
+++ pkgs/aerial-sddm-theme/default.nix
@@ -1,10 +1,20 @@
-{ sources, qtgraphicaleffects, qtmultimedia, qtquickcontrols, mkDerivation }:
-
+{
+  sources,
+  qtgraphicaleffects,
+  qtmultimedia,
+  qtquickcontrols,
+  gstreamer,
+  phonon,
+  phonon-backend-gstreamer,
+  gst_all_1,
+  mkDerivation,
+}:
 mkDerivation {
   inherit (sources.aerial-sddm-theme) src pname version;
-  propagatedUserEnvPkgs = [qtgraphicaleffects qtmultimedia qtquickcontrols];
+  propagatedUserEnvPkgs = [gst_all_1.gst-plugins-good phonon phonon-backend-gstreamer qtgraphicaleffects qtmultimedia qtquickcontrols];
   installPhase = ''
     mkdir -p $out/share/sddm/themes
     cp -r ./. $out/share/sddm/themes/aerial-sddm-theme
+    cp ${./theme.conf} $out/share/sddm/themes/aerial-sddm-theme/theme.conf.user
   '';
 }
