755d1e991f3731aa22817dc4365401f2e7739635
marsupialgutz <mars@possums.xyz>
2022-07-18 13:44:41 -0400

    ough

--- sys/generic.nix
+++ sys/generic.nix
@@ -40,6 +40,9 @@ in {
       };
       waybar = super.waybar.overrideAttrs (oldAttrs: {
         mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+        patchPhase = ''
+          substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"hyprctl dispatch workspace \" + name_; system(command.c_str());"
+        '';
       });
     })
     inputs.powercord-overlay.overlay
