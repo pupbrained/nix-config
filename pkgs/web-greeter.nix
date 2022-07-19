7938a539d35c8100ad83bc10d020435f80324dc6
ReplayCoding <replaycoding@gmail.com>
2022-07-19 18:48:02 -0400

    adonosndiandiosnionaiosniODniodosdjuauusaudeifropo[kpowibklkfpfh0r h9rhuitor iowbiofnowndwrtgpitmxpmdiffeifodnpnpsnfpsnspd

--- pkgs/web-greeter.nix
+++ pkgs/web-greeter.nix
@@ -1,32 +1,51 @@
 {
   stdenv,
+  lib,
   web-greeter-src,
   nodejs,
   rsync,
   pkg-config,
   python3Packages,
+  python3,
   gobject-introspection,
   lightdm,
   xorg,
   libsForQt5,
   callPackage,
-}:
-stdenv.mkDerivation {
-  pname = "web-greeter";
-  version = web-greeter-src.rev;
-  src = web-greeter-src;
-  dontWrapQtApps = true;
-  nativeBuildInputs = [nodejs rsync pkg-config python3Packages.pyqt5];
-  buildInputs = with python3Packages; [lightdm pygobject3 pyqt5 pyqtwebengine ruamel_yaml pyinotify gobject-introspection xorg.libX11 libsForQt5.qtwebengine];
-  __noChroot = true;
-  buildPhase = ''
-    HOME=$(mktemp -d)
-    # fuck it
-    npm exec --package=typescript -- make
-  '';
-  installPhase = ''
-    make install
-  '';
-  DESTDIR = placeholder "out";
-  PREFIX = "/";
-}
+}: let
+  myPython = python3.withPackages (p:
+    with p; [
+      pygobject3
+      pyqt5
+      pyqtwebengine
+      ruamel_yaml
+      pyinotify
+    ]);
+in
+  stdenv.mkDerivation
+  {
+    pname = "web-greeter";
+    version = web-greeter-src.rev;
+    src = web-greeter-src;
+    dontWrapQtApps = true;
+    nativeBuildInputs = [nodejs rsync pkg-config python3Packages.pyqt5];
+    buildInputs = [
+      lightdm
+      gobject-introspection
+      xorg.libX11
+      libsForQt5.qtwebengine
+    ];
+    __noChroot = true;
+    buildPhase = ''
+      sed -i 's/python3/${lib.strings.escape ["/"] (toString myPython)}\/bin\/python3/g' Makefile
+
+      HOME=$(mktemp -d)
+      # fuck it
+      npm exec --package=typescript -- make
+    '';
+    installPhase = ''
+      make install
+    '';
+    DESTDIR = placeholder "out";
+    PREFIX = "/";
+  }
