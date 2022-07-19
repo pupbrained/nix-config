b6ba361400d7d300325a8c1becf47cde8fb6d29b
marsupialgutz <mars@possums.xyz>
2022-07-19 17:37:05 -0400

    waydorid

--- sys/nix/configuration.nix
+++ sys/nix/configuration.nix
@@ -20,6 +20,11 @@
     extraModprobeConfig = "options hid_apple fnmode=1";
   };
 
+  virtualisation = {
+    waydroid.enable = true;
+    lxd.enable = true;
+  };
+
   networking = {
     hostName = "nix";
     useDHCP = false;
